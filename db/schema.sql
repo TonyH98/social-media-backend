3edd93c41f4c944ae68621452e7dd0f75039d3fa
TonyH98 <tonyhoang@pursuit.org>
2023-06-24 13:11:26 -0400

    Put the tables in the schema file

--- db/schema.sql
+++ db/schema.sql
@@ -2,3 +2,25 @@ DROP DATABASE IF EXISTS social;
 CREATE DATABASE social;
 
 \c social;
+
+DROP TABLE IF EXISTS users; 
+CREATE TABLE users(
+    id SERIAL PRIMARY KEY,
+    username TEXT UNIQUE NOT NULL,
+    firstname TEXT NOT NULL,
+    lastname TEXT NOT NULL,
+    email TEXT UNIQUE NOT NULL,
+    phonenumber TEXT NOT NULL,
+    profile_img TEXT,
+    banner_img TEXT,
+    DOB TEXT,
+    bio TEXT,
+    profile_name TEXT,
+    password TEXT NOT NULL
+);
+
+DROP TABLE IF EXISTS posts;
+CREATE TABLE posts(
+    users_id INTEGER REFERENCES users(id);
+    posts VARCHAR(500) NOT NULL;
+)
