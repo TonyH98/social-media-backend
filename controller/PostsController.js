7b711f77fbe9ae9f5f490e2fa8a8069cccc34223
TonyH98 <tonyhoang@pursuit.org>
2023-06-29 22:52:50 -0400

    Second createReaction function removed'

--- controller/PostsController.js
+++ controller/PostsController.js
@@ -1,6 +1,6 @@
 const express = require("express")
 
-const {getAllPosts, createPost, updatePost, deletePosts, createReaction, getReaction, createReaction} = require("../queries/Posts")
+const {getAllPosts, createPost, updatePost, deletePosts, createReaction, getReaction} = require("../queries/Posts")
 
 
 const posts = express.Router({mergeParams: true})
