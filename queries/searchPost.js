5afe2295345d90830dd6e32d474a9b687ac54e81
TonyH98 <tonyhoang@pursuit.org>
2023-07-03 21:14:32 -0400

    searchPost query was given more information regarding the post details and the creator of the post

--- queries/searchPost.js
+++ queries/searchPost.js
@@ -3,10 +3,20 @@ const db = require("../db/dbConfig")
 const searchPost = async (tagName) => {
   try {
     const search = await db.any(
-      `SELECT ph.*, p.*, h.tag_names
+      `SELECT ph.*, h.tag_names,
+      json_build_object(
+        'content', p.content,
+        'date_created', p.date_created
+      )as posts_details,
+      json_build_object(
+        'username', p.user_name,
+        'profile_name', u.profile_name,
+        'profile_img', u.profile_img
+      ) As creator_details
       FROM post_hashtags ph
       JOIN posts p ON p.id = ph.post_id
      JOIN hashtags h ON h.id = ph.hashtag_id
+     JOIN users u ON u.id = ph.user_id
       WHERE h.tag_names = $1`,
       tagName
     );
