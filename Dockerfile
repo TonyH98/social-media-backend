21d4502f9a72fd61d0f8bb7358d4815a5d90017b
TonyH98 <tonyhoang@pursuit.org>
2024-01-15 19:12:44 -0500

    Able to create a node.js and postgres contianer

--- Dockerfile
+++ Dockerfile
@@ -6,6 +6,8 @@ COPY . .
 
 RUN npm install
 
+RUN npm rebuild bcrypt --build-from-source
+
 EXPOSE 3333
 
 CMD ["node", "server.js"]
--- db/dbConfig.js
+++ db/dbConfig.js
@@ -1,22 +1,21 @@
-const pgp = require("pg-promise")()
+const pgp = require("pg-promise")();
+require("dotenv").config();
 
-require("dotenv").config()
-
-const {DATABASE_URL, PG_HOST, PG_PORT, PG_DATABASE, PG_USER} = process.env;
+const { DATABASE_URL, PG_HOST, PG_PORT, PG_DATABASE, PG_USER } = process.env;
 
 const cn = DATABASE_URL
-?{
-    connectionString: DATABASE_URL,
-    max:  30,
-}:
-{
-host: PG_HOST,
-port: PG_PORT,
-database: PG_DATABASE,
-user: PG_USER,
-password: process.env.PG_PASSWORD
-};
+  ? {
+      connectionString: DATABASE_URL,
+      max: 30,
+    }
+  : {
+      host: PG_HOST || "postgres_container", 
+      port: PG_PORT || 5432,
+      database: PG_DATABASE,
+      user: process.env.PG_USER,
+      password: process.env.PG_PASSWORD,
+    };
 
-const db = pgp(cn)
+const db = pgp(cn);
 
-module.exports = db
+module.exports = db;
